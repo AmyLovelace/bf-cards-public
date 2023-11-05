53730b6e40c666bc6dba23fda3172904dc2c1b22
Amanecer Cabrera <amanecer.luz.cabrera@gmail.com>
2023-11-05 04:17:32 -0300

    Create CreateCardInUseCaseTest.java

---
+++ src/test/java/com/ms/bf/card/application/CreateCardInUseCaseTest.java
@@ -0,0 +1,51 @@
+package com.ms.bf.card.application;
+
+import com.ms.bf.card.adapter.kafka.KafkaProducerPort;
+import com.ms.bf.card.application.port.in.CreateIn;
+import com.ms.bf.card.application.usecase.CreateCardInUseCase;
+import com.ms.bf.card.domain.Card;
+import com.ms.bf.card.config.exception.GenericException;
+import com.ms.bf.card.config.ErrorCode;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.messaging.MessagingException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
+
+public class CreateCardInUseCaseTest {
+
+    @Mock
+    private KafkaProducerPort kafkaProducerPort;
+
+    @Mock
+    private Card card;
+
+    @InjectMocks
+    private CreateCardInUseCase createCardInUseCase;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void testCreate() throws GenericException, MessagingException {
+        when(kafkaProducerPort.sendMessage(card)).thenReturn(0);
+
+        Integer result = createCardInUseCase.create(card);
+
+        assertEquals(0, result);
+    }
+
+    @Test
+    public void testCreateThrowsException() throws GenericException, MessagingException {
+        when(kafkaProducerPort.sendMessage(card)).thenThrow(new MessagingException("Error al generar el mensaje"));
+
+        assertThrows(GenericException.class, () -> createCardInUseCase.create(card));
+    }
+}
