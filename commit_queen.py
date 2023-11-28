2c9ede19048b991d377ae5daf9f9be77afb719b4
Amanecer Cabrera <amanecer.luz.cabrera@gmail.com>
2023-11-28 01:58:38 -0300

    Update commit_queen.py

--- commit_queen.py
+++ commit_queen.py
@@ -9,10 +9,15 @@ for n in range(random.randint(1, 6)):
     to_write = f'{random.choice(messages)}\n' * 100
 
     if n > 4:
-        os.system(f'echo "{to_write}" > bin/generated-{random.randint(1, 10)}.txt')
+        # Check if the file exists
+        if not os.path.exists('bin/generated-{random.randint(1, 10)}.txt'):
+            # Create the file
+            os.system(f'echo "{to_write}" > bin/generated-{random.randint(1, 10)}.txt')
+
+        # Add the file to the staging area
         os.system(f'git add bin/generated-{random.randint(1, 10)}.txt')
     else:
+        # Append to the file
         os.system(f'echo "{to_write}" >> bin/generated-{random.randint(1, 10)}.txt')
-        os.system(f'git add bin/generated-{random.randint(1, 10)}.txt')
 
     os.system('git commit -m "{q(random.choice(messages))}"')
