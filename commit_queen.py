dd72f76a0dac2383f2e06ca4e15b3d373a0e1d78
Amanecer Cabrera <amanecer.luz.cabrera@gmail.com>
2023-11-28 14:36:03 -0300

    Update commit_queen.py

--- commit_queen.py
+++ commit_queen.py
@@ -1,18 +1,19 @@
 import random
 import os
 
-q = lambda z: ''.join([v, v + ''.join(random.choice(list(map(chr, range(768, 815)))) for i in range(int(random.normalvariate(10, 5))))][v.isalpha()] for v in z)
+# zalgoification stolen from a code golf example, could I format this better? yes. will I? no.
+q=lambda z:''.join([v,v+''.join(random.choice(list(map(chr,range(768,815))))for i in range(int(random.normalvariate(10,5))))][v.isalpha()]for v in z)
 
-messages = ["update readme.md", "update", "initial commit"]
+# these messages taken from a list of most common commit messages
+messages=[
+	"initial commit",
+	"update readme.md",
+	"update",
+	"bump version",
+	"updates"	
+]
 
-for n in range(random.randint(1, 6)):
-    to_write = f'{random.choice(messages)}\n' * 100
-
-    if n > 4:
-        os.system(f'echo "{to_write}" > bin/dump-{random.randint(1, 10)}.txt')
-    else:
-        os.system(f'echo "{to_write}" >> bin/dump-{random.randint(1, 10)}.txt')
-
-    os.system('git add .')
-    os.system(f'git commit -m "{q(random.choice(messages))}"')
-    os.system('git push origin commitQueen')  # Push changes to the remote repository
+# I couldn't get the bash for loop to run in the github action, so... here we are.
+for n in range(random.randint(1, 10)):
+	message = q(random.choice(messages))
+	os.system(f'git commit -m "{message}" --allow-empty')
